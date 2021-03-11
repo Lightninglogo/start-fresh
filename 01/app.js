1929222a888f05bb628bc006841ad8da8888d572
Lightninglogo <macbookair@maegbug-eeoui-MacBook-Air.local>
2021-03-11 16:47:42 +0900

    01 수업과제

---
+++ 01/app.js
@@ -0,0 +1,56 @@
+console.log('app.js');
+
+//var name = '김소형';
+//var name = '김영소';
+//onsole.log('name');
+
+//var var = 'test';
+//onsole.log('test');
+
+//function log(str){
+  //  console.log(str);
+// }
+
+//   var log = function(str){
+ //   console.log(str);
+//  };
+//   log('hello');
+
+//var image = {
+  //  width: 100,
+    //height: 100,
+   // name: '1.png',
+//   };
+
+//var image1 = [1,2,3,4,5,6,'a',{}];
+//image1[0] = 'a';
+//var data = null;
+//var data1 = '';
+//var data2;
+//console.log(data,data1,data2); 
+ 
+//var imagewidth = 100;
+//var imageheigth = 100;
+//var imagename = '1.png';
+//console.log(image,image1);
+
+//console.log(1 + 2);
+//comsole.log('a' + 'b');
+
+//var count = 1;
+//var number = '2';
+//console.log(count + number);
+
+//console.log('' == '0');
+//console.log(0 === '', 0 == '');
+
+//var isHero = true;
+//if(isHero){
+ //   console.log('hero');
+// }
+
+// console.log(typeof 1);
+// console.log(typeof []);
+// console.log(typeof '1');
+// console.log(typeof {});
+
